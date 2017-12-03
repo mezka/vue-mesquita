147e7cc0ae7e42e3b1546b323abc156602397efd
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2017-12-03 17:03:58 -0300

    added contact email server code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var mailgunPassword = require('./keys.js');
 
 var app = module.exports = express();
 
@@ -12,56 +13,39 @@ app.use(bodyParser.urlencoded(
 var port = 59787;
 
 app.post('/api/contact', function (req, res) {
-
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    nodemailer.createTestAccount((err, account) => {
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass  // generated ethereal password
-            }
-        });
-
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-            to: 'emiliano_mesquita@hotmail.com, psychatog36@gmail.com', // list of receivers
-            subject: 'Hello ✔', // Subject line
-            text: req.body.text, // plain text body
-        };
-
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        });
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.mailgun.org',
+        port: 587,
+        auth: {
+            user: 'postmaster@sandboxf2530c8c879d4cd2b703815ca697b446.mailgun.org',
+            pass: mailgunPassword
+        }
     });
 
-    console.log(req.body.text);
+    var mailText = 'Nombre completo: ' + req.body.name + '\nTelefono: ' + req.body.phone + '\nE-mail: ' + req.body.email + '\nHorario para contactarlo: ' + req.body.timetocontact + '\nComentarios: ' + req.body.text;
+    
+    var mailOptions = {
+        from: 'mesquita.emiliano@gmail.com',
+        to: 'psychatog36@gmail.com',
+        subject: 'Contacto desde www.mesquita.com.ar',
+        text: req.body.text,
+    };
 
-    res.status(200).json(req.body);
+
+
+
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.json({yo: 'error'});
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        };
+    });
 });
 
-
-app.listen(port, function() {
-    console.log('Listening on port: ', port, '\n');
-});
-
-
-
-
-
-
+app.listen(port, function(req, res){
+    console.log('Listening on: ', port);
+})
