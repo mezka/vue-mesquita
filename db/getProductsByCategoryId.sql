a08a181974381a6feeba505ef491bb8052d36e14
mezka <mezka@mesquitahome.com>
2018-02-02 11:46:08 -0300

    presupuestador is working now, still need to add quantities

--- db/getProductsByCategoryId.sql
+++ db/getProductsByCategoryId.sql
@@ -1,7 +1,7 @@
-SELECT ProductCategories.productId,Categories.categoryName AS productCategory, Products.productName, array_to_json(ARRAY[]::varchar[])::JSONB AS productSelectedAccesories, json_agg(json_build_object('productid', p2.productId, 'productname', p2.productName, 'productprice', p2.productPrice)) AS productAccesories, Products.productPrice FROM Mesquita.ProductCategories AS ProductCategories
+SELECT ProductCategories.productId,Categories.categoryName AS productCategory, Products.productName, array_to_json(ARRAY[]::varchar[])::JSONB AS productSelectedAccessories, json_agg(json_build_object('productid', p2.productId, 'productname', p2.productName, 'productprice', p2.productPrice)) AS productAccessories, Products.productPrice FROM Mesquita.ProductCategories AS ProductCategories
 INNER JOIN Mesquita.Products AS Products ON Products.productId = ProductCategories.productId
 INNER JOIN Mesquita.Categories AS Categories ON ProductCategories.categoryId = Categories.categoryId
 INNER JOIN Mesquita.ProductAccesories AS ProductAccesories ON ProductAccesories.productId1 = Products.productId
 INNER JOIN Mesquita.Products AS p2 ON ProductAccesories.productId2 = p2.productId
 WHERE ProductCategories.categoryId = $1
-GROUP BY ProductCategories.productId, ProductCategories.categoryId, Categories.categoryName, Products.productName, productSelectedAccesories, Products.productPrice
+GROUP BY ProductCategories.productId, ProductCategories.categoryId, Categories.categoryName, Products.productName, productSelectedAccessories, Products.productPrice
--- src/views/Presupuestador.vue
+++ src/views/Presupuestador.vue
@@ -1,38 +1,55 @@
 <template>
-    
-    <section class="presupuestador-wrapper container">
-                <div v-for="i in selectedLength(selected)" :key="i">
-                    <div class="row">
-                        <div class="col-xs-5">
-                            <select class="form-control" v-model="selected[i]">
-                                <option v-for="(product, ind) in products" v-bind:value="product" :key="ind">
-                                    {{ product.productname }}
-                                </option>
-                            </select>
-                        </div>
+    <div>
+          <p>Cart</p>
+  <div v-for="item in shoppingCart" class="cart">
+    <span v-if="item.selectedProdId > -1">
+          {{ prodData[item.selectedProdId].productname }} - {{ prodData[item.selectedProdId].productprice }} $
+    </span>
+    <ul class="selected-a">
+      <li class="li-a" v-for="a in item.productselectedaccessories" v-if="a.productid > -1">
+        {{ a.productname }} - {{ a.productprice }} $
+      </li>
+    </ul>
+  </div>
 
-                        <div class="col-xs-1 col-xs-offset-6">
-                            <p>{{ selected[i]? '$' + selected[i].productprice : ''}}</p>
-                        </div>
-                    </div>
-                    <template v-if="selected[i]">
-                        <div class="row" v-for="j in selectedLength(selected[i]['productselectedaccesories'])" :key="j">
-                            <div class = "col-xs-6 col-xs-offset-3">
-                                <select class="form-control" v-model="selected[i]['productselectedaccesories'][j]">
-                                    <option v-for="(productaccesory, index) in selected[i].productaccesories" :value="productaccesory" :key="index">
-                                        {{ productaccesory.productname }}
-                                    </option>
-                                </select>
-                            </div>
-                            <div class="col-xs-1 col-xs-offset-2">
-                                <p>{{ selected[i]? '$' + selected[i].productprice : ''}}</p>
-                            </div>
-                        </div>
-                    </template>
-                </div>                
-        <p>{{selected}}</p>
-    </section>
+  <hr>
+  <div>
+    <div class="cart-item" :key="item.id" v-for="(item, cartIdx) in shoppingCart">
+      <div class="product">
+        Product
+        <select v-model="item.selectedProdId">
+          <option :key="prodIdx" v-for="(prod, prodIdx) in prodData" :value="prodIdx"> {{ prod.productname }} - {{ prod.productprice }} $</option>
+        </select>
 
+        <button type="button" class="btn btn-danger" @click="removeProd(cartIdx)">
+          -
+        </button>
+      </div>
+      <hr>
+      <div class="prod-accessory" v-if="item.selectedProdId > -1">
+        Accessories
+        <button type="button" class="btn btn-success" @click="addAccessory(cartIdx)">
+          +
+        </button>
+      </div>
+      <div class="accessory-list" v-if="item.selectedProdId > -1">
+        <div class="accessory-item" :key="idx" v-for="(a, idx) in shoppingCart[cartIdx].productselectedaccessories">
+          <select v-model="shoppingCart[cartIdx].productselectedaccessories[idx]">
+            <option :key="aIdx" v-for="(a, aIdx) in prodData[item.selectedProdId].productaccessories" :value="a"> {{ a.productname }} - {{ a.productprice }} $</option>
+          </select>
+          <button type="button" class="btn btn-danger" @click="removeAccessory(cartIdx, idx)">
+            -
+          </button>
+        </div>
+      </div>
+
+    </div>
+
+    <button type="button" class="btn btn-success" @click="addProduct">
+      +
+    </button>
+  </div>
+    </div>
 </template>
 
 <script>
@@ -56,34 +73,100 @@ export default {
 
     data() {
         return {
-            selected: [],
-            products: [],
+            idGenerator: 0,
+            prodData: [],
+            shoppingCart: [],
         };
     },
 
-    methods: {
-        setProducts(products) {
-            this.products = products;
+    computed: {
+        cart() {
+            return this.shoppingCart;
         },
-        selectedLength(array) {
-            let count = 0;
+    },
 
-            for (let i = 0; i < array.length; i += 1) {
-                if (array[i]) {
-                    count += 1;
-                }
-            }
+    methods: {
+        addProduct() {
+            this.shoppingCart.push({
+                // eslint-disable-next-line
+                id: this.idGenerator++,
+                selectedProdId: -1,
+                productselectedaccessories: [],
+            });
+        },
 
-            if (count === 0) {
-                return 1;
-            }
+        addAccessory(idx) {
+            this.shoppingCart[idx].productselectedaccessories.push({
+                productid: -1,
+                productname: '',
+                productprice: -1,
+            });
+        },
 
-            return count + 1;
+        removeProd(idx) {
+            this.shoppingCart.splice(idx, 1);
+        },
+
+        removeAccessory(cartIdx, idx) {
+            this.shoppingCart[cartIdx].productselectedaccessories.splice(
+                idx,
+                // eslint-disable-next-line
+                1
+            );
+        },
+
+        setProducts(products) {
+            console.log(products);
+            this.prodData = products;
         },
     },
 };
 </script>
 
 <style lang="scss" scoped>
+.body {
+    font-family: Tahoma;
+    font-size: 11px;
+}
 
+.cart {
+    border: 1px solid grey;
+    margin: 20px;
+    padding: 5px;
+}
+
+.cart-item {
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid lightgrey;
+    align-self: center;
+}
+
+.product {
+    display: flex;
+    justify-content: flex-start;
+}
+
+select {
+    margin: 5px;
+    font-size: 11px;
+}
+
+.prod-accessory {
+    display: flex;
+    margin: 5px;
+}
+
+ul.selected-a {
+    font-size: 10px;
+}
+
+button.btn {
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+    margin: 0 5px;
+}
 </style>
